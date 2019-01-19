ae18bc0c51644e7f632f6df0ca7f5009fa48398f
rxs4498 <ryyan_j_smith@homedepot.com>
2019-01-19 15:40:19 -0500

    working copy that returns index.html webpage

--- src/main/java/com/ryyanj/servlets/HelloServlet.java
+++ src/main/java/com/ryyanj/servlets/HelloServlet.java
@@ -1,10 +1,8 @@
 package com.ryyanj.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,8 +11,8 @@ public class HelloServlet extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    RequestDispatcher view =req.getRequestDispatcher("resources/index.html");
-    view.forward(req, resp);
+    RequestDispatcher view =req.getRequestDispatcher("index.html");
+    view.include(req, resp);
   }
 
 
