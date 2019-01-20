743c2564723e70605cbcd72cfe69a6d5abe01480
rxs4498 <ryyan_j_smith@homedepot.com>
2019-01-19 19:57:12 -0500

    update

--- src/main/java/com/ryyanj/servlets/HelloServlet.java
+++ src/main/java/com/ryyanj/servlets/HelloServlet.java
@@ -3,6 +3,7 @@ package com.ryyanj.servlets;
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +15,13 @@ public class HelloServlet extends HttpServlet {
     RequestDispatcher view =req.getRequestDispatcher("index.html");
     view.include(req, resp);
   }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        RequestDispatcher view =req.getRequestDispatcher("index.html");
+        view.include(req, resp);
+    }
 
 
 }
